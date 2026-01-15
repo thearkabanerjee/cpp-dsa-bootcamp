8562c63348759e42041df31656a66ad3ce6f74a4
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 23:11:06 +0530

    cpp in bootcamp

---
+++ codeforces/max2sums.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int marks;
+    cin >> marks;
+    if (marks >= 35 ){
+        cout << "Pass"<< endl;
+    }
+    else {
+        cout << "Fail"<< endl;
+    }
+
+    return 0;
+}
