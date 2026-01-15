872bdcf3cd315ccc582887312dfb17c0f8a034d5
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 22:37:47 +0530

    cpp in bootcamp

---
+++ relational_operators.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    bool ans = - 1243243423; // true --> 1 ; only 0 means false
+    bool ans1 = 5== 5; // true  --> 1
+    bool ans2 = 6== 5; // false --> 0
+
+    cout << ans1 << endl;
+    cout << ans2 << endl;
+    cout << ans << endl;
+    return 0;
+}
