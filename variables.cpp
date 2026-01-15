59370c105572f73e6cb54523b1933333debe9f96
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 21:43:29 +0530

    cpp in bootcamp

---
+++ variables.cpp
@@ -0,0 +1,11 @@
+// variables
+
+# include <iostream>
+using namespace std;
+
+int main (){
+    string name = "Arka"; // string is the data type of name
+    int age = 20; // int is the data type of age
+
+    cout << name << "-" << age << endl;
+}
