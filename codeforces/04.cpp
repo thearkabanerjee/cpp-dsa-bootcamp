eb16a4de14b4746283a617c6b65064d4563eb4e8
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 22:17:30 +0530

    cpp in bootcamp

---
+++ codeforces/04.cpp
@@ -0,0 +1,14 @@
+// print z
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << "*****" <<'\n';
+    cout << "   * "<< '\n';
+    cout << "  *  " <<'\n';
+    cout << " *   " <<'\n';
+    cout << "*****" <<'\n';
+
+    return 0;
+}
