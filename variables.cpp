2da3734c90a802015126dd9ffeb5bb69434d5a2a
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 22:06:49 +0530

    cpp in bootcamp

--- variables.cpp
+++ variables.cpp
@@ -6,6 +6,29 @@ using namespace std;
 int main (){
     string name = "Arka"; // string is the data type of name
     int age = 20; // int is the data type of age
-
-    cout << name << "-" << age << endl;
+    bool whatisthere = true;
+    cout << name << "-" << age <<  endl;
 }
+
+
+//data types
+
+/*
+    int = integer 
+    float = 
+    double = 
+
+    long long = 
+    long = 
+    char = one character only
+    bool = true / false
+
+*/
+
+
+/* variables must be declared in this
+age != Age (case sensitive)
+no spaces
+name should be meaningful
+
+*/
