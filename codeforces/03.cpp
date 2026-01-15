c60c0d8cf5ccb1904e8525cc3a5d716b7f9dbf4b
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 22:13:51 +0530

    cpp in bootcamp

---
+++ codeforces/03.cpp
@@ -0,0 +1,13 @@
+// triangle
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << "*****"<< endl;
+    cout << "****"<< endl;
+    cout << "***"<< endl;
+    cout << "**"<< endl;
+    cout << "*"<< endl;
+    return 0;
+}
