f691ce0909a330d1b06e3db412e8546d3f55d8e8
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 22:46:23 +0530

    cpp in bootcamp

---
+++ codeforces/05.cpp
@@ -0,0 +1,30 @@
+// print table of 5
+
+
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout << "5 * 1 = 5" << endl;
+    cout << "5 * 2 = 10" << endl;
+    cout << "5 * 3 = 15" << endl;
+    cout << "5 * 4 = 20" << endl;
+    cout << "5 * 5 = 25" << endl;
+    cout << "5 * 6 = 30" << endl;
+    cout << "5 * 7 = 35" << endl;
+    cout << "5 * 8 = 40" << endl;
+    cout << "5 * 9 = 45" << endl;
+    cout << "5 * 10 = 50" << endl;
+
+    return 0;
+}
+// 5 * 1 = 5
+// 5 * 2 = 10
+// 5 * 3 = 15
+// 5 * 4 = 20
+// 5 * 5 = 25
+// 5 * 6 = 30
+// 5 * 7 = 35
+// 5 * 8 = 40
+// 5 * 9 = 45
+// 5 * 10 = 50
