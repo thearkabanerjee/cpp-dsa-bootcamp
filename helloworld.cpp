6a8062a88c8768c1abeb66c773a8b3e2eb0f93d8
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 21:23:57 +0530

    cpp in bootcamp

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main(){
     cout<< "hello world"<< endl;
-}
+    cout << "hello arka";
+    cout << endl; // this is how it can be written as well
 
+    cout << "arka"<< "banerjee"<< "is"<< "the"<<"best"<< endl; // also another way of doing it
+}
+// endl is what helps to help iin 
 // cout helps to print something on the screen
