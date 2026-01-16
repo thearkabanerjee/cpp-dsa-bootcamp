fb854388a580fd66ddc056693086bfbffe505aae
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-16 11:18:05 +0530

    cpp in bootcamp

---
+++ codeforces/factor.cpp
@@ -0,0 +1,18 @@
+//factor
+
+
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+    if (a % b==0){
+        cout << "Yes"<< endl;
+    }
+    else{
+        cout << "No"<< endl;
+    }
+    return 0;
+}
