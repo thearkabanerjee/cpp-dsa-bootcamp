e78e9c66fffdab426c45bf5c959c69e8715e8cc2
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-16 11:59:17 +0530

    cpp in bootcamp

---
+++ icpc_uni_codeforces/diff.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    long long a, b, c, d;
+    cin >> a>> b>> c>> d;
+    long long ans = (a*b)-(c*d);
+    cout << "Difference = "<< ans<< endl;
+    return 0;
+}
