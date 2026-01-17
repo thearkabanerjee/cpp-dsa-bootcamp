6b3b4f4b48a883e0689e67718841448642d41e87
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-17 21:14:03 +0530

    cpp in bootcamp

--- icpc_uni_codeforces/contest/digitssummation.cpp
+++ icpc_uni_codeforces/contest/digitssummation.cpp
@@ -3,11 +3,9 @@ using namespace std;
 
 int main(){
     long long a, b ;
-    cin >> a>> b;
-
-    int aend, bend;
-    aend = a % 10;
-    bend = b % 10;
+    cin >> a>> b;;
+    int aend = a % 10;
+    int bend = b % 10;
     cout << aend +bend << endl;
 
     return 0;
