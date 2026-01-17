65835563046fde84024bd4a2853a68066d1a0f53
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-17 14:19:09 +0530

    cpp in bootcamp

---
+++ icpc_uni_codeforces/contest/mama.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    long long counter =0;
+    long long n;
+    cin >> n;
+    for (int i = 0; i<=n; i++){
+        counter +=i;
+        
+    }
+    cout << counter << endl;
+    
+    return 0;
+}
