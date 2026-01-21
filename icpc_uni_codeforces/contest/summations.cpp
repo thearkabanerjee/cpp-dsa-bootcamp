50715674ad93e2bef77914cc12deb31e4130819c
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-21 10:40:02 +0530

    cpp in bootcamp

---
+++ icpc_uni_codeforces/contest/summations.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+
+int main (){
+
+    long long num;
+    cin >> num;
+    long long sum = (num * (num+1))/2;
+
+    cout << sum << endl;
+    return 0;
+}
