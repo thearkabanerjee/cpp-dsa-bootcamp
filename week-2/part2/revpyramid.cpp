e93769a3919619295b733cdee64f0b6779e2c693
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-22 22:33:01 +0530

    cpp in bootcamp

---
+++ week-2/part2/revpyramid.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    for (int i = a; i>=1; i--){
+        for (int j = 1; j<=i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
