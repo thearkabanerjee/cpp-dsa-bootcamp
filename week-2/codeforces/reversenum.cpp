ccd5dcfce05d5e60af752f8fc1baf2e86d762a0e
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-22 21:33:33 +0530

    cpp in bootcamp

--- week-2/codeforces/reversenum.cpp
+++ week-2/codeforces/reversenum.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main(){
     long long a;
     cin >> a;
-    
+    long long revnum;
+
     return 0;
 }
+
+// removing last digit from number n /= 10 
---
+++ week-2/codeforces/sumofdigits.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    long long num;
+    cin >> num;
+    int sum = 0;
+    
+    return 0;
+}
