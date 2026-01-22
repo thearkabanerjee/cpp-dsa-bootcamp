d429cd6e5672f512711043e8a8ba0818e9fd894c
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-22 21:17:29 +0530

    cpp in bootcamp

---
+++ week-2/codeforces/exponent.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+    long long ans = 1;
+    for (int i = 1 ; i<=b; i++ ){
+        ans *= a;
+    }
+    cout << ans << endl;
+
+    return 0;
+}
