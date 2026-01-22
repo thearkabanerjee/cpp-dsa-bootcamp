3517d474f87076b957e73109f84575f5eab05a20
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-22 21:41:17 +0530

    cpp in bootcamp

--- week-2/codeforces/sumofdigits.cpp
+++ week-2/codeforces/sumofdigits.cpp
@@ -6,6 +6,10 @@ int main(){
     long long num;
     cin >> num;
     int sum = 0;
-    
+    while (num > 0){
+        sum += num %10;
+        num /= 10;
+    }
+    cout << sum << endl;
     return 0;
 }
