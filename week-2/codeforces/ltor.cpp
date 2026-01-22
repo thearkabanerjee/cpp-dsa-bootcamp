0859a192000673fa9ebe9ade13bc2c0fd2d2db6d
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-22 20:50:52 +0530

    cpp in bootcamp

---
+++ week-2/codeforces/ltor.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    long a , b;
+    cin >> a >> b;
+
+    while (a <=b){
+        cout << a << " ";
+        a++;
+    }
+
+    return 0;
+}
