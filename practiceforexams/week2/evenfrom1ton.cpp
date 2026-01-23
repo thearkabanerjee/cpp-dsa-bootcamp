6ab13a3bd1a728dfcd25759dc0abb4555321fb97
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:05:57 +0530

    cpp in bootcamp

--- practiceforexams/week2/evenfrom1ton.cpp
+++ practiceforexams/week2/evenfrom1ton.cpp
@@ -6,12 +6,10 @@ int main(){
     int a;
     cin >> a;
     for (int i = 1; i <= a; i++){
-
         if (i % 2 == 0){
             cout << i << " ";
         }
     }
     cout << endl;
-
     return 0;
 }
