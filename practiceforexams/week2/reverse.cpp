8ec12f3a455bb516d6f2e86a84253bd3ad42cff3
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:51:14 +0530

    cpp in bootcamp

---
+++ practiceforexams/week2/reverse.cpp
@@ -0,0 +1,12 @@
+# include <iostream>
+# include <algorithm> // this is necessary btw
+using namespace std;
+
+int main(){
+    string a;
+    // for (int i = 0; i<=)
+    cin >> a;
+    reverse(a.begin(), a.end()); // reverses the string
+    cout << a<< endl;
+    return 0;
+}
