a1941d3f9aa8e0942616f518a12310df7d5f11df
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:40:51 +0530

    cpp in bootcamp

---
+++ practiceforexams/week2/exponential.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a , b;
+    long long ans = 1;
+    cin >> a>> b;
+    for (int i = 1; i <= b; i++){
+        ans *= a;
+    }
+    cout << ans << endl;
+    return 0;
+}
