62b031fd31c1e9d2eedd71fd8f84f5afd1cdf7b9
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:18:44 +0530

    cpp in bootcamp

---
+++ practiceforexams/week2/uppercase.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    for (char letters = 'A'; letters<= 'Z'; letters++){
+        cout << letters << " ";
+    }
+    cout << endl;
+    return 0;
+}
