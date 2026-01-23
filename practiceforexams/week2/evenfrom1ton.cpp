ee3653de9286b68dac63356f2b5d0ff532553000
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:05:28 +0530

    cpp in bootcamp

---
+++ practiceforexams/week2/evenfrom1ton.cpp
@@ -0,0 +1,17 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    cin >> a;
+    for (int i = 1; i <= a; i++){
+
+        if (i % 2 == 0){
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
