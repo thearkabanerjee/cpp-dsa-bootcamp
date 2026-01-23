cdbd5e5d477591d043e1d6a043212d0e551bb6a0
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-23 11:16:04 +0530

    cpp in bootcamp

---
+++ practiceforexams/week2/ltor.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    long long a, b;
+    cin >> a>> b;
+    for (int i = a; i <= b; i++){ // could have also used a while loop
+
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
