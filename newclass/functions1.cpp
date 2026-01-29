b96e2246a813afc877c82fb73ae6c8e8b2160fbb
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-29 20:47:44 +0530

    codeforces

--- newclass/functions1.cpp
+++ newclass/functions1.cpp
@@ -1,8 +1,22 @@
 # include <iostream>
 using namespace std;
 
+
+// bionomial distribution
+
+long long factorial(int num){
+    long long a = 1;
+    for (int i = 1; i<=num; i++){
+        a *= i;
+    }
+    return a;
+}
+
+
 int main(){
-    
-    
+    int a ,b;
+    cin >> a >> b;
+    long long m = factorial(a)/(factorial(a-b)* factorial(b));
+    cout << m << endl;
     return 0;
 }
