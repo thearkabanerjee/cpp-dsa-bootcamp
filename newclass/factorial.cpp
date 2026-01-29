cf936d96eca88f64ad9333bd7fe204c2cc7ba4df
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-29 20:41:27 +0530

    codeforces

--- newclass/factorial.cpp
+++ newclass/factorial.cpp
@@ -2,16 +2,39 @@
 using namespace std;
 
 
-int  main(){
+// int  main(){
     
-    int a;
-    cin >> a;
-    long long b =1;
+//     int a;
+//     cin >> a;
+//     long long b =1;
     
-    for (int i = 1; i<=a; i++){
-        b *= i;
+//     for (int i = 1; i<=a; i++){
+//         b *= i;
+//     }
+    
+//     cout << b << endl;
+//     return 0;
+// }
+
+
+// other way of factorial is this
+
+long long factorial(int num){
+    long long a =1;
+    for (int i = 1; i<=num ; i++){
+        a *=i;
     }
     
-    cout << b << endl;
+    return a;
+}
+
+
+int main(){
+    int a;
+    cin >> a;
+    
+    long long b = factorial(a);
+    cout << b<< endl;
+    
     return 0;
 }
