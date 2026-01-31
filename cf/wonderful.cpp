3608ee5a292f04531a426b1b22a34c75b31ee3d0
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-31 11:21:03 +0530

    codeforces

---
+++ cf/wonderful.cpp
@@ -0,0 +1,20 @@
+// a number is wonderful if it is an odd number and it's binary is a palindrome
+
+
+# include <iostream>
+using namespace std;
+
+string oddchecker(int num){
+    return (num % 2 ==0? "yes": "no");
+}
+
+string binpalindromechecker(int num){
+    
+}
+
+
+int main(){
+    
+    
+    return 0;
+}
