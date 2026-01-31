702e56bb63118d1fe87e4d096e533861e8ffa5cc
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-31 09:08:48 +0530

    codeforces

--- functions/02.cpp
+++ functions/02.cpp
@@ -1,15 +1,20 @@
 # include <iostream>
 using namespace std;
 
-int main(){
-    int a;
-    cin >> a;
+void divisors(int a){
     for (int i = 1; i<=a; i++){
         if (a%i ==0){
             cout << i<< " ";
         }
     }
     cout << endl;
+}
+
+
+int main(){
+    int a;
+    cin >> a;
     
+    divisors(a);
     return 0;
 }
