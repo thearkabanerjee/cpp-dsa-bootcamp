622774844613243f5f81ac36d182ff59f26d104e
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-31 08:43:24 +0530

    codeforces

--- functions/01.cpp
+++ functions/01.cpp
@@ -5,7 +5,12 @@ using namespace std;
 
 
 int main(){
+    int a;
+    cin >> a;
     
+    for (int i =1; i<=a; i++){
+        cout << "I am learning functions"<< endl;
+    }
     
     return 0;
 }
