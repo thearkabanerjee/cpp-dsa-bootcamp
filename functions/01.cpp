099249ef6b1327f1cac50d1f993e65962930f688
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-31 09:06:51 +0530

    codeforces

--- functions/01.cpp
+++ functions/01.cpp
@@ -2,15 +2,29 @@
 using namespace std;
 
 
+void printer (int num){
+    for (int i = 1; i<= num; i++){
+        cout << "I am learning functions"<< endl;
+    }
+}
+
 
 
 int main(){
-    int a;
+    int a ;
     cin >> a;
     
-    for (int i =1; i<=a; i++){
-        cout << "I am learning functions"<< endl;
-    }
-    
+    printer(a);
     return 0;
 }
+
+// int main(){
+//     // int a;
+//     // cin >> a;
+    
+//     // for (int i =1; i<=a; i++){
+//     //     cout << "I am learning functions"<< endl;
+//     // }
+    
+//     return 0;
+// }
