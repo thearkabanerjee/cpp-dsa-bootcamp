e1bf60ebcf27d4d77da07621eb7aeb07a4efe4da
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-31 09:11:52 +0530

    codeforces

--- functions/03.cpp
+++ functions/03.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 
-int main(){
-    int a;
-    cin >> a;
-    
+void printrevdivisors(int a){
     for(int i = a; i>=1; i--){
         if (a % i ==0){
             cout << i<< " ";
         }
     }
     cout << endl;
+}
+
+int main(){
+    int a;
+    cin >> a;
+    
+    printrevdivisors(a);
     
     return 0;
 }
