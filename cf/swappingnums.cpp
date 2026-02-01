72a3f6aa332894955480e70377c45b88b2cb3bee
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-02 01:23:59 +0530

    codeforces

--- cf/swappingnums.cpp
+++ cf/swappingnums.cpp
@@ -1,14 +1,18 @@
 # include <iostream>
 using namespace std;
 
-int main(){
-    int a, b;
-    cin >> a>> b;
+void swapper(int a, int b){
     int temp = a;
     a = b;
     b = temp;
+    cout << a<< " "<< b<<endl;
+}
+ // better one honestly
+int main(){
+    int a, b;
+    cin >> a>> b;
+    swapper(a,b);
     
-    cout << a<< " "<< b<<  endl;
     
     return 0;
 }
