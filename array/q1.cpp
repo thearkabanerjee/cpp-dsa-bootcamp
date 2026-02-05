7675ce534e3bd177a05cb456e74bc08fb7f401c9
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-05 21:02:50 +0530

    c++

---
+++ array/q1.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    cin >> a;
+    
+    long long arr[a];
+    
+    for (int i = 0; i<a; i++){
+        cin >> arr[i];
+    }
+    for (int i = a-1; i>=0; i--){
+        cout << arr[i]<< " ";
+    }
+    cout << endl;
+    return 0;
+}
