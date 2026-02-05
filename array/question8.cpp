6b69d7631b8e2dc02530251ee04987cd52965382
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-05 22:47:21 +0530

    c++

--- array/question8.cpp
+++ array/question8.cpp
@@ -1,33 +1,29 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
+int main() {
+    int m;
+    cin >> m;
 
-int main(){
-    int a;
-    cin >> a;
-    
-    long long arr[a];
-    for (int i=0; i<a; i++){
-        cin >> arr[i];
-    }
-    
-    int count0= 0;
-    int count1= 0;
-    for (int i = 0; i<a; i++){
-        if (arr[i]==0){
-            count0 += 1;
+    for (int t = 0; t < m; t++) {
+        int a;
+        cin >> a;
+
+        long long arr[a];
+        for (int i = 0; i < a; i++) {
+            cin >> arr[i];
         }
-        if (arr[i] ==1){
-            count1 += 1;
+
+        int count0 = 0, count1 = 0;
+        for (int i = 0; i < a; i++) {
+            if (arr[i] == 0) count0++;
+            else if (arr[i] == 1) count1++;
         }
+
+        for (int i = 0; i < count0; i++) cout << "0 ";
+        for (int i = 0; i < count1; i++) cout << "1 ";
+        cout << endl;
     }
-    
-    for  (int i = 0; i<count0; i++){
-        cout << "0"<< " ";
-    }
-    for  (int i = 0; i<count0; i++){
-        cout << "1"<< " ";
-    }
-    cout << endl;
-    return 0;
+
+    return 0;  // ← moved here
 }
