51c9355c85b633c9660d445f26410cc7403c9c3a
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-08 10:44:59 +0530

    c++

---
+++ whatiwanna/array/printarrayinreverse.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    
+    int arr[a];
+    for (int i =0 ; i < a; i++){
+        cin >> arr[i];
+    }
+    
+    for (int i = a-1; i >=0; i--){
+        cout << arr[i]<< " ";
+    }
+    cout << endl;
+    return 0;
+}
