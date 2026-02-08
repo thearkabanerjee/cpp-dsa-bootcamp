a8640a73443d916f307202b6c88da302ee766fc7
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-08 09:45:28 +0530

    js

---
+++ whatiwanna/intro/first5char.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    char a = 'a';
+    while (a <= 'e'){
+        cout << a << " "; 
+        a++;
+    }
+    cout << endl;
+    return 0;
+}
