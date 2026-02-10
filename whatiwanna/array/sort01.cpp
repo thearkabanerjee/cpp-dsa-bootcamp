056d70a7091adc26aad581bec4f5c70a40e7c6f8
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 11:39:12 +0530

    c++

--- whatiwanna/array/sort01.cpp
+++ whatiwanna/array/sort01.cpp
@@ -9,10 +9,38 @@ int main(){
     for (int i = 0; i <a; i++){
         int m;
         cin >> m;
+        int arr[m];
         
         for (int i = 0; i<m; i++){
-            cin <<
+            cin >> arr[i];
         }
+        
+        
+        int count0 = 0;
+        int count1 = 0;
+        
+        for (int i = 0; i <m; i++){
+            if (arr[i] == 0){
+                count0 += 1;
+            }
+            else if (arr[i] == 1){
+                count1 += 1;
+            }
+        }
+        for (int i = 0; i<count0; i++){
+            cout << 0 << " ";
+        }
+        for (int i = 0; i <count1; i++){
+            if (i < count1){
+                cout << 1<< " ";
+            }
+            else{
+                cout << 1;
+            }
+        }
+        
+        cout << endl;
+        
     }
     
     
