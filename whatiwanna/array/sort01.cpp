306c3f05f742abe88cfef23cb009652df86dad00
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-10 11:28:59 +0530

    c++

---
+++ whatiwanna/array/sort01.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    cin >> a;
+    
+    for (int i = 0; i <a; i++){
+        int m;
+        cin >> m;
+        
+        for (int i = 0; i<m; i++){
+            cin <<
+        }
+    }
+    
+    
+    
+    return 0;
+}
