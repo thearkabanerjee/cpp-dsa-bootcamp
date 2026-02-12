2f3fb37c82e3a85ecd02ee25080b853ce18335f5
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-12 21:00:35 +0530

    codeforces contest 2

---
+++ otherme/2darray/columnorder.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+    
+    int arr[a][b];
+    
+    for (int i = 0 ; i< a; i++){
+        for (int j = 0; j < b; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    for (int j = 0; j < b; j++){
+        for (int i = 0; i< a; i++){
+            cout << arr[i][j] << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
