007fb94a3cec31142b57edc954ae11befd55aeb4
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-12 22:25:29 +0530

    c++

--- otherme/strings/tolower.cpp
+++ otherme/strings/tolower.cpp
@@ -1,12 +1,35 @@
-#include <cctype>
+// # include <iostream>
+// using namespace std;
+
+// int main(){
+//     char a;
+//     cin >> a;
+    
+//     cout << (char)tolower(a)<< endl;
+    
+    
+//     return 0;
+// }
+
+
 # include <iostream>
 using namespace std;
+
 
 int main(){
     char a;
     cin >> a;
     
-    cout << (char)tolower(a)<< endl;
+    if (a >= 'A' && a<='Z'){
+        int ascii_a = int(a);
+        int ascii_lower_a = ascii_a + 32;
+        
+        
+        cout << char(ascii_lower_a)<< endl;
+    }
+    else {
+        cout << a<< endl;
+    }
     
     
     return 0;
