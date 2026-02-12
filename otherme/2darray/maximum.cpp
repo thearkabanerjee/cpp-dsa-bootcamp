b10261a42d2e1ad2e95c8c41d12eef54aa7c97ae
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-12 21:08:30 +0530

    codeforces contest 2

---
+++ otherme/2darray/maximum.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a, b;
+    cin >> a>> b;
+    
+    int arr[a][b];
+    
+    for (int i = 0; i<a ; i++){
+        for (int j = 0; j < b; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    int max = arr[0][0];
+    for (int i = 0; i< a; i++){
+        for (int j = 0; j < b; j++){
+            if (arr[i][j] >= max){
+                max = arr[i][j];
+            }
+        }
+    }
+    
+    cout << max<< endl;
+    return 0;
+}
