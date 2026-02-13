ce583e04b771c3a8d18e4e03279dd66801164e18
thearkabanerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-13 20:30:16 +0530

    c++

---
+++ otherprobs/uniqueelements.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    int arr[5000];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        int count = 0;
+
+        for (int j = 0; j < N; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        if (count == 1) {
+            cout << arr[i] << " ";
+        }
+    }
+
+    cout << endl;
+    return 0;
+}
