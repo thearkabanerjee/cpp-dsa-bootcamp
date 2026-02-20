d3d3b2f5a6f35ce6fd5689e2083a0dcda3fc906b
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-02-20 10:07:01 +0530

    c++

---
+++ practicesearch/linearsearch.cpp
@@ -0,0 +1,28 @@
+# include <iostream>
+using namespace std;
+
+
+int main(){
+    int a;
+    cin >> a;
+    int arr[a];
+    
+    
+    for (int i = 0; i < a; i++){
+        cin >> arr[i];
+    } 
+    int b;
+    cin >> b;
+    
+    
+    string flag = "False";
+    for (int i = 0 ; i < a; i++){
+        if (arr[i] == b){
+            flag = "True";
+        }
+    }
+    
+    
+    cout << flag << endl;
+    return 0;
+}
